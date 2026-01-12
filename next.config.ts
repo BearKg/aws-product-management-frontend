29731ed6d13ac3bb4f01c485fa2e618c17e542b5
hahuynhvan <hahuynhhuy159@gmail.com>
2026-01-12 15:16:04 +0700

    10/1

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'productmanagementstack-images.s3.amazonaws.com',
+        hostname: 'productmanagementstack-images.s3.ap-southeast-1.amazonaws.com',
         port: '',
         pathname: '/**',
       },
